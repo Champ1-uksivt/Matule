5fed028ab4028c6e8fa101427a1741e236d65e78
uksivt <uksivt@iMac-uksivt-6.local>
2025-02-18 10:10:28 +0500

    initt

---
+++ Matule/MatuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatuleApp.swift
+//  Matule
+//
+//  Created by uksivt on 18.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MatuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatuleTests/MatuleTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatuleTests.swift
+//  MatuleTests
+//
+//  Created by uksivt on 18.02.2025.
+//
+
+import Testing
+@testable import Matule
+
+struct MatuleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
