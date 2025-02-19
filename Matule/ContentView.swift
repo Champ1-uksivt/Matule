37d9874cc11554ba8b5cb70a2a81ff3184449df1
evgeny <uksivt@iMac-uksivt-6.local>
2025-02-19 08:30:09 +0500

    init

--- Matule/ContentView.swift
+++ Matule/ContentView.swift
@@ -6,13 +6,11 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
         }
         .padding()
